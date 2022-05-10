dcbe0dc76167d3b0e051d1edb3cbbf0460571b93
alireza <alireza.elecat@gmail.com>
2022-05-10 16:55:38 +0200

    cleaning up the code

---
+++ scripts/vs_navigationNode.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+import rospy
+from vs_nodeHandler import vs_nodeHandler 
+
+    
+if __name__ == '__main__':
+    #  initializing vs navigator node 
+    rospy.init_node('vs_navigator', anonymous=False)
+    rospy.loginfo("#[VS] Visual_servoing navigator node running ...")
+    
+    # instantiating navigator class
+    nodeHandler = vs_nodeHandler()
+
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        rospy.logwarn("#[VS] Shutting down Phenobot Controller")
