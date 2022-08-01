268b039132e1e57c02bc3e863fe31d91bdd2d0d9
Alaa <ejalaa12@gmail.com>
2022-08-01 10:18:39 +0200

    update launch file to ros2 and install it

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+from glob import glob
+import os
+
 from setuptools import setup
 
 package_name = 'visual_multi_crop_row_navigation'
@@ -10,6 +13,7 @@ setup(
         ('share/ament_index/resource_index/packages',
          ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.xml'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
